09330f58c56c336a64f14f7886e9a148c03c6d82
OlegZhdanov <stalker08@gmail.com>
2020-04-09 12:48:11 +0500

    exercise 1 complete

---
+++ lesson_1-1.py
@@ -0,0 +1,11 @@
+var_str = 'hello'
+var_int = 5
+var_float = 3.2
+print('String = ', var_str, '\nInteger = ', var_int, '\nFloat = ', var_float)
+
+random_str = input('Введите произвольную строку\n')
+random_int = input('Введите произвольное целое число\n')
+random_float = input('Введите произвольное дробное число\n')
+
+print('Ваша строка = ', random_str, '\nВаше целое число = ', int(random_int), '\nВаше дробное число = ',
+      float(random_float))
